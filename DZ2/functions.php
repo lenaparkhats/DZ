9c4dcc51d53914122b4deeeab18dd9bbd33bbc99
Helen Parkhats <lenaparkhats@gmail.com>
2018-06-09 12:57:10 +0300

    correct09/06

--- DZ2/functions.php
+++ DZ2/functions.php
@@ -6,44 +6,73 @@ function task1($arr, $x){
         foreach ($arr as $ind) {
             echo '<p>' . $ind . '</p>';
         }
-
     } else{
         $u = implode("", $arr);
         return $u;
-    }
+    };
 };
 
-echo task1(['aaaa', 'ddddd', 'ccccc'] , true);
-
-echo "<br> <br>";
 
 function task2 ($str, $num){
     echo "Задание 2 <br>";
     $x = func_get_args();
-
-       $len = array_slice($x, 1);
-        echo implode(" + ", $len);
-
-    echo "= " . array_sum($x);
-    echo "<br>";
-
-}
-task2('+', 3, 2, 3, 4, 5);
-
-echo "<br> <br>";
-
-function task3 ($x, $y = 0){
-    echo "Задание 3 <br>";
-    if($y != 0) {
-        for ($i = 1; $i <= $x; $i++) {
-            echo "<table><tr>";
-            for ($u = 1; $u <= $y; $u++) {
-                echo "<td>" . $u * $i . " " . "</td>";
+    $t = 1;
+    switch ($str){
+        case "+":
+            $len = array_slice($x, 1);
+            echo implode(" + ", $len);
+            echo "= " . array_sum($len);
+            echo "<br>";
+            break;
+        case "*":
+            $len = array_slice($x, 1);
+            echo implode(" * ", $len);
+            for($i = 1; $i < count($x); $i++){
+                $t *= $x[$i];
             }
-            echo "</tr>";
+            echo "= $t";
+            break;
+        case "-":
+            $len = array_slice($x, 1);
+            echo implode(" - ", $len);
+            for($i = 1; $i < count($x); $i++){
+                $t -= $x[$i];
+            }
+            echo "= $t";
+            break;
+        case "/":
+            $len = array_slice($x, 1);
+            echo implode(" / ", $len);
+            for($i = 1; $i < count($x); $i++){
+                $t /= $x[$i];
+            }
+            echo "= $t";
+            break;
+    };
+};
+
+function task3 ($x = 0, $y = 0){
+    echo "Задание 3 <br>";
+
+    if (is_int($x)){
+        if (is_int($y)){
+            if($x !=0 and $y != 0) {
+                for ($i = 1; $i <= $x; $i++) {
+                    echo "<table><tr>";
+                    for ($u = 1; $u <= $y; $u++) {
+                        echo "<td>" . $u * $i . " " . "</td>";
+                    }
+                    echo "</tr>";
+                }
+            } elseif ($x != 0 and $y == 0) {
+                echo "Введите второй аргумент";
+            } else{
+                echo "Введите аргументы";
+            }
+        }else{
+            echo "Второе число не целое. Введите целое число";
         }
-    } else {
-        echo "Введите второй аргумент";
+    }else{
+        echo "Первое число не целое. Введите целое число";
     }
 }
-task3(8, 8);
--- DZ2/index.php
+++ DZ2/index.php
@@ -1,2 +1,12 @@
 <?php
 include "functions.php";
+
+echo task1(['aaaa', 'ddddd', 'ccccc'] , true);
+
+echo "<br> <br>";
+
+task2('/', 3, 2, 3, 4, 5);
+
+echo "<br> <br>";
+
+task3(1, 1.1);
