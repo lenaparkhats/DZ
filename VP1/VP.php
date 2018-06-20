6d541dd0a40b0e5d6f73ade492504e371cdf0079
Helen Parkhats <lenaparkhats@gmail.com>
2018-06-20 17:41:30 +0300

    20/06

--- VP1/VP.php
+++ VP1/VP.php
@@ -1,10 +1,4 @@
 <?php
-$dsn = "mysql:host=localhost;dbname=LF;charset=utf8";
-$pdo = new PDO($dsn, 'root', '');
-//$stmt = $pdo ->query('SELECT * FROM user');
-//$result = $stmt ->fetchAll(PDO::FETCH_ASSOC);
-
-//print_r($result);
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
@@ -15,22 +9,43 @@ $appt = $_POST['appt'];
 $floor = $_POST['floor'];
 $comment = $_POST['comment'];
 
-$em = "SELECT name FROM user WHERE email = '$email'";
+$dsn = "mysql:host=localhost;dbname=LF;charset=utf8";
+$pdo = new PDO($dsn, 'root', '');
 
-$stmt = $pdo ->query($em);
+//$id_user = $pdo->prepare("SELECT id FROM user WHERE email = :email");
+//$id_user->bindParam(':email', $email, PDO::PARAM_STR);
+//$id_user->execute();
 
-$id_user = "SELECT id FROM user WHERE email = $email";
-
+$id_user = "SELECT id FROM user WHERE email = '$email'";
 $idU = $pdo ->query($id_user);
+$data = $idU->fetchAll(PDO::FETCH_OBJ);
 
-$quer = "INSERT INTO user (Name, email, tel, street, home, part, appt, floor, comment) VALUES ($name, $phone, $email, $street, $home, $part, $appt, $floor, $comment)";
+$stmt = $pdo->prepare("INSERT INTO user (name, email, tel, street, home, part, appt, floor, comment) VALUES (:name, :email, :phone, :street, :home, :part, :appt, :floor, :comment)");
 
-$order = "INSERT INTO orders (id_user, name_user, street, home, part, appt, floor, comment) VALUES ($id_user, $name, $street, $home, $part, $appt, $floor, $comment)";
+$stmt->bindParam(':name', $name, PDO::PARAM_STR);
+$stmt->bindParam(':email', $email, PDO::PARAM_STR);
+$stmt->bindParam(':phone', $phone, PDO::PARAM_INT);
+$stmt->bindParam(':street', $street, PDO::PARAM_STR);
+$stmt->bindParam(':home', $home, PDO::PARAM_STR);
+$stmt->bindParam(':part', $part, PDO::PARAM_STR);
+$stmt->bindParam(':appt', $appt, PDO::PARAM_STR);
+$stmt->bindParam(':floor', $floor, PDO::PARAM_INT);
+$stmt->bindParam(':comment', $comment, PDO::PARAM_STR);
 
-if ($stmt == null){
-    $stmt = $pdo ->query($quer);
-    $stmt = $pdo ->query($order);
+$order = $pdo->prepare("INSERT INTO orders (id_user, name_user, street, home, part, appt, floor, comment) VALUES (:id_user, :name_user, :street, :home, :part, :appt, :floor, :comment)");
+
+$order->bindParam(':id_user', $id_user, PDO::PARAM_INT);
+$order->bindParam(':name_user', $name, PDO::PARAM_STR);
+$order->bindParam(':street', $street, PDO::PARAM_STR);
+$order->bindParam(':home', $home, PDO::PARAM_STR);
+$order->bindParam(':part', $part, PDO::PARAM_STR);
+$order->bindParam(':appt', $appt, PDO::PARAM_STR);
+$order->bindParam(':floor', $floor, PDO::PARAM_INT);
+$order->bindParam(':comment', $comment, PDO::PARAM_STR);
+
+if ($data == null){
+    $stmt->execute();
+    $order->execute();
 } else {
-    $idU = $pdo ->query($id_user);
-    $stmt = $pdo ->query($order);
+    $order->execute();
 };
